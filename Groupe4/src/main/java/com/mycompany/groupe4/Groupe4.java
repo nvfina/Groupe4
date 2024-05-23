7d201a0b1f07c62714c2a078a57e0c34582a3a66
nvfina <122808671+nvfina@users.noreply.github.com>
2024-05-23 13:45:03 +0000

    Add files via upload

---
+++ Groupe4/src/main/java/com/mycompany/groupe4/Groupe4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.groupe4;
+
+/**
+ *
+ * @author yanaf
+ */
+public class Groupe4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
